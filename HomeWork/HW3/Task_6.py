fd26a5585e4b17e05fa0c34fd3c45d8a0b4bc89a
BorisLukashenok <jnardis@yandex.ru>
2022-12-16 02:34:56 +0300

    DZ and Seminar 3 Complet fix 2

--- HomeWork/HW3/Task_6.py
+++ HomeWork/HW3/Task_6.py
@@ -1,10 +1,10 @@
-# Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность 
+# Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность
 # (сдвиг - циклический) на |K| элементов вправо, если K – положительное и влево, если отрицательное.
 
 from random import randint as rnd
 
 number_list = [rnd(0, 10) for i in range(int(input("Количество элементов? ")))]
 print(number_list)
-sdvig = int(input('Введите сдвиг: '))
+sdvig = int(input("Введите сдвиг: "))
 sdvig = sdvig % len(number_list)
-print(number_list[-sdvig:]+number_list[:-sdvig])
+print(number_list[-sdvig:] + number_list[:-sdvig])
