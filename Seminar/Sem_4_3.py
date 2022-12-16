70542c65d09698b86d6de3274db7b42e25337075
BorisLukashenok <jnardis@yandex.ru>
2022-12-17 00:04:03 +0300

    DZ and Seminar 4 Complet

--- Seminar/Sem_4_3.py
+++ Seminar/Sem_4_3.py
@@ -2,13 +2,6 @@
 # этих двух чисел.
 
 # НОК = (a*b) / НОД(a,b) (алгоритм евклида)
-def Nodss( a, b):
-    while a != 0 and b != 0:
-        if a > b:
-            a = a % b
-        else:
-            b = b % a
-    return a + b
 
 n = int(input())
 m = int(input())
