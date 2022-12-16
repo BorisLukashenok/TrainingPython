81be3b89c25a1939b5fdb7dfb27bffc67f72e84f
BorisLukashenok <jnardis@yandex.ru>
2022-12-17 00:57:59 +0300

    DZ and Seminar 4 Complet and fix 1

--- HomeWork/HW4/Task_6.py
+++ HomeWork/HW4/Task_6.py
@@ -4,33 +4,38 @@ def print_dikt(slovar):
             print(f"{i}: {slovar[i]}")
 
 
+def rev(n):
+    if n == 0:
+        n = 1
+    else:
+        n = 0
+    return n
+
+
 dicts = dict()
 lists = list()
-while True:
+flag = True
+while flag:
     stroka = input()
     if stroka != "":
         lists = stroka.split(" ")
-        if lists[0] not in dicts:
-            dicts[lists[0]] = [lists[1]]
-        else:
-            dicts[lists[0]] += [lists[1]]
-        if lists[1] not in dicts:
-            dicts[lists[1]] = [lists[0]]
-        else:
-            dicts[lists[1]] += [lists[0]]
-    elif stroka == "":
-        break
+        for i in range(2):
+            if lists[i] not in dicts:
+                dicts[lists[i]] = [lists[rev(i)]]
+            else:
+                dicts[lists[i]] += [lists[rev(i)]]
+    else:
+        flag = not flag
+
 print("Друзья первого рукопожатия")
 print_dikt(dicts)
 dicts2 = {}
-lists = list()
 for i in dicts:
+    dicts2[i] = list()
     for j in dicts[i]:
-        lists += list(dicts.get(j))  # добавляем друзей друзей
-        while i in lists:  # удаляем текушую фамилию
-            lists.remove(i)  
-    lists = list(set(lists))  # удаляем повторы
-    dicts2[i] = lists
-    lists = list()
+        dicts2[i] += dicts.get(j)  # добавляем друзей друзей
+        while i in dicts2[i]:  # удаляем текушую фамилию
+            dicts2[i].remove(i)
+    dicts2[i] = list(set(dicts2[i]))  # удаляем повторы
 print("Друзья второго рукопожатия")
 print_dikt(dicts2)
