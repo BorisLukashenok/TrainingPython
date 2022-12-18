1cf47047d338eaf3799878d6c3a045442f4bf351
BorisLukashenok <jnardis@yandex.ru>
2022-12-19 02:31:39 +0300

    DZ and Seminar 4 Complet and fix 5

--- HomeWork/HW4/Task_5.py
+++ HomeWork/HW4/Task_5.py
@@ -3,13 +3,17 @@
 file_1 = "file_for_task_5_1.txt"
 file_2 = "file_for_task_5_2.txt"
 file_out = "file_for_task_5_out.txt"
+minus = '-'
 
 
 def prepare(file_name):
     with open(file_name, "r") as inp:
-        pol_ls = inp.read().replace(" = 0", "").split(" + ")
+        pol_ls = inp.read().replace(" = 0\n", "").split(" ")
+    if len(pol_ls) % 2 != 0:
+        pol_ls.insert(0, '+')
+    print(pol_ls)
     pol_dk = dict()
-    for i in range(len(pol_ls)):
+    for i in range(1, len(pol_ls), 2):
         if "X" in pol_ls[i]:
             stroka = pol_ls[i][: pol_ls[i].find("X")]
             if "^" in pol_ls[i]:
@@ -18,7 +22,10 @@ def prepare(file_name):
                 key = 1
             pol_dk[key] = 1 if stroka == "" else int(stroka)
         else:
-            pol_dk[0] = int(pol_ls[i])
+            key = 0
+            pol_dk[key] = int(pol_ls[i])
+        if pol_ls[i-1] == minus:
+            pol_dk[key] *= -1
     return pol_dk
 
 
@@ -27,11 +34,6 @@ polynom_2 = prepare(file_2)
 polynom_sum = dict()
 stepen = max(max(i for i in polynom_1), max(i for i in polynom_2))
 for i in range(stepen + 1):
-    # if polynom_1.get(i) == None:
-    #     polynom_1[i] = 0
-    # if polynom_2.get(i) == None:
-    #     polynom_2[i] = 0
-    # polynom_sum[i] = polynom_1[i] + polynom_2[i]
     polynom_sum[i] = (polynom_1[i] if polynom_1.get(i) != None else 0) + (
         polynom_2[i] if polynom_2.get(i) != None else 0)
 lists = list()
@@ -51,7 +53,10 @@ for i in range(stepen, -1, -1):
         else:
             lists += [f"{temp}"]
 stroka = f"{' + '.join(lists)} = 0"
-
+if minus in stroka:
+    if stroka[0] == minus:
+        stroka = '+ '+stroka
+    stroka = stroka.replace('+ -', '- ')
 print(stroka)
 with open(file_out, "w") as out:
     out.writelines(stroka)
